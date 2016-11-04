bb6fc100747fd9d5bff7febda9dcd944ba36caec
shangboyang <yang8701@gmail.com>
2016-11-04 09:53:24 +0800

    init

---
+++ app/app.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import route from './config/route';
+import store from './config/store';
+import './css/resets.less'; //重置浏览器默认样式
+// import './css/data-flex.min.css'; //css布局文件
+import 'flex-css-layout';
+// import './css/style.less'; //css文件
+// import './iconfont/iconfont.css'; //字体图标文件
+
+store.subscribe(function () {
+    //    console.log(store.getState());
+});
+ReactDOM.render(
+    <Provider store={store}>
+        {route}
+    </Provider>,
+    document.body.appendChild(document.createElement('div'))
+);
---
+++ build/webpack.base.conf.js
@@ -0,0 +1,53 @@
+var webpack = require('webpack');
+var path = require("path");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var projectRoot = path.resolve(__dirname, '../');
+
+module.exports = {
+    entry: {
+        app: './app/app', //编译的入口文件
+    },
+    output: {
+        publicPath: './build/', //服务器根路径
+        path: path.resolve(__dirname, '../dist/static'), //编译到当前目录
+        filename: '[name].js' //编译后的文件名字
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /^node_modules$/,
+            loader: 'babel?presets=es2015'
+        }, {
+            test: /\.css$/,
+            exclude: /^node_modules$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader')
+        }, {
+            test: /\.less/,
+            exclude: /^node_modules$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader!less-loader')
+        }, {
+            test: /\.(eot|woff|svg|ttf|woff2|gif|appcache)(\?|$)/,
+            exclude: /^node_modules$/,
+            loader: 'file-loader?name=[name].[ext]'
+        }, {
+            test: /\.(png|jpg)$/,
+            exclude: /^node_modules$/,
+            loader: 'url?limit=20000&name=[name].[ext]' //注意后面那个limit的参数，当你图片大小小于这个限制的时候，会自动启用base64编码图片
+        }, {
+            test: /\.jsx$/,
+            exclude: /^node_modules$/,
+            loaders: ['jsx', 'babel?presets[]=es2015,presets[]=react']
+        }]
+    },
+    plugins: [
+        new webpack.DefinePlugin({ //编译成生产版本
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new ExtractTextPlugin('[name].css')
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx'], //后缀名自动补全
+    }
+};
